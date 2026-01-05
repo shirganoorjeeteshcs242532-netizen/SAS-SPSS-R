74445955b7bcf43c8097883ed47a5e7cca6a64ac
shirganoorjeeteshcs242532-netizen <shirganoorjeeteshcs242532@gmail.com>
2026-01-05 12:13:25 +0530

    S125 M2 P7.R

--- S125 M2 P7.R
+++ S125 M2 P7.R
@@ -1,17 +1,21 @@
-# Practical 7: One-Way ANOVA (Heart Disease Dataset)
+# Load data
+d <- read.csv("Heart_Disease_Prediction.csv")
+d$Sex <- as.factor(d$Sex)
 
-# Load dataset
-data <- read.csv("Heart_Disease_Prediction.csv")
+# Means
+GM <- mean(d$Cholesterol)
+m  <- tapply(d$Cholesterol, d$Sex, mean)
+n  <- table(d$Sex)
 
-# Check column names
-colnames(data)
+# Sum of Squares
+SS_between <- sum(n * (m - GM)^2)
+SS_total   <- sum((d$Cholesterol - GM)^2)
+SS_within  <- SS_total - SS_between
 
-# Convert categorical variable to factor
-data$Sex <- as.factor(data$Sex)
+# Mean Squares (FORMULA)
+MS_between <- SS_between / (length(m) - 1)
+MS_within  <- SS_within / (nrow(d) - length(m))
 
-# Apply One-Way ANOVA
-anova_result <- aov(Cholesterol ~ Sex, data = data)
-
-# Display ANOVA table
-summary(anova_result)
-
+# F statistic (EXACT IMAGE FORMULA)
+F <- MS_between / MS_within
+F
