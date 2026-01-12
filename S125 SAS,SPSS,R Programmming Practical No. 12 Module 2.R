eaf393984b9c50dcb0a734a8d6ad0eaaacbdd535
shirganoorjeeteshcs242532-netizen <shirganoorjeeteshcs242532@gmail.com>
2026-01-12 12:11:11 +0530

    Add files via upload

--- S125 SAS,SPSS,R Programmming Practical No. 12 Module 2.R
+++ S125 SAS,SPSS,R Programmming Practical No. 12 Module 2.R
@@ -1,14 +1,17 @@
-# Read CSV file
-data <- read.csv("Prostate Cancer.csv", check.names = TRUE)
+# Create dataset
+data <- data.frame(
+  Hours_Studied = c(2, 4, 6, 8, 10),
+  Previous_Scores = c(45, 55, 65, 75, 85),
+  Sleep_Hours = c(6, 7, 8, 7, 6),
+  Practice_Test_Score = c(40, 50, 60, 70, 80),
+  Performance_Index = c(50, 60, 70, 80, 90)
+)
 
-df <- data[, c("radius",
-               "texture",
-               "perimeter",
-               "area",
-               "smoothness")]
-
+# View dataset
+data
 # Generate correlation matrix
-cor_matrix <- cor(df)
+cor_matrix <- cor(data)
 
 # Display correlation matrix
-print(cor_matrix)
+cor_matrix
+round(cor_matrix, 2)
